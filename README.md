c16a1c3ea3c3c4ae2785c1a8344f5303953c6deb
denghuiquan <980352129@qq.com>
2023-03-18 19:53:55 +0800

    init the v0.0.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# gatsby-transformer-xml
+
+Project Url: [https://github.com/denghuiquan/gatsby-transformer-xml](https://github.com/denghuiquan/gatsby-transformer-xml)
+
+
+Source plugin for Gatsby to transformer  xml content from the gatsby-source-filesystem to javascript object. And save then to the graphql data level.
+
+### A gatsby-config.js example:
+
+```js
+module.exports = {
+  plugins: ['gatsby-transformer-xml']
+}
+
+```
---
+++ gatsby-node.js
@@ -0,0 +1,42 @@
+const { randomUUID } = require('crypto')
+const { parseStringPromise } = require('xml2js')
+const createNodeHelper = require('gatsby-node-helpers').createNodeHelpers
+
+async function onCreateNode (
+  { createNodeId, createContentDigest, node, actions, loadNodeContent },
+  configOptions
+) {
+  const { createNode } = actions
+  if (node.internal.mediaType === 'application/xml') {
+    const { name } = node
+    const content = await loadNodeContent(node)
+    const data = await parseStringPromise(content, {
+      explicitArray: false,
+      explicitRoot: false
+    })
+    // 1. 构建数据节点对象 allXml
+    const { createNodeFactory } = createNodeHelper({
+      typePrefix: `Xml${name.replace(/^./, name[0].toUpperCase())}`,
+      createNodeId,
+      createContentDigest
+    })
+
+    // 2. 根据数据节点对象创建节点
+    const createNodeObject = createNodeFactory('')
+    // Todo: 考虑xml内部组织结构的多样性
+    const objs =
+      Object.entries(data).length === 1 ? Object.values(data).at(0) : data
+
+    Array.isArray(objs)
+      ? objs.forEach(async item => {
+          item.id = item.id || item['$']?.id || randomUUID()
+
+          await createNode(createNodeObject(item))
+        })
+      : await createNode(createNodeObject(objs))
+  }
+}
+
+module.exports = {
+  onCreateNode
+}
